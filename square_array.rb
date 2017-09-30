8185ab6b983e36765a336b6787bf030edf2cce9e
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-09-30 18:49:57 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,7 @@
 def square_array(array)
   # your code here
   new_array = []
-  array.each do |number|
-    expnum = number * number
-    new_array << expnum
-
-  end
-  new_array
+  array.each {|number|
+    new_array << number ** 2}
+    new_array
 end
